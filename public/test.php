0b0204e0ca43c9b3e148a093fb6154bd66b0858e
manojrammurthy <manrox.drag@gmail.com>
2014-05-13 07:28:17 -0400

    few changes

--- public/test.php
+++ public/test.php
@@ -1,6 +1,10 @@
 <?php
 include '../model/dbcon.php';
 $db=dbopen();
+$ccname=$_POST['ccname'];
+$states=$_POST['states'];
+$ccname=$_POST['ccname'];
+
 $addstory = "insert into storytrack(fid,ccname,state,receiveddate,issuetopic,storydescription) values (UUID(),'amar','karnataka',now(),'health','myhealth is not well today')";
 $new = $db->query($addstory);
 if(!$new)
