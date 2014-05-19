0b867807a6c0cd04d5964be9181a0ff843311e0c
StephanieBr <stephaniebrowne123@gmail.com>
2014-05-19 16:03:57 +0530

    added view by .php pages

---
+++ public/topic.php
@@ -0,0 +1,26 @@
+<?php
+mysql_connect('localhost', 'root', '123');
+mysql_select_db('videovol');
+?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>topics</title>
+</head>
+<body>
+
+<?php
+$query = mysql_query("SELECT * FROM storytrack"); // Run the query
+
+echo '<label>View By:</label><select name="issuetopic">'; // Opens the drop down box
+
+// Loops through the query results, outputing the options one by one
+while ($row = mysql_fetch_array($query)) {
+   echo '<option value="'.$row['issuetopic'].'">'.$row['issuetopic'].'</option>';
+}
+
+echo '</select>';// Closes drop down box
+?>
+</body>
+</html>
