b97135e2e5e1317a05c9b2fa51375121682dcf8c
StephanieBr <stephaniebrowne123@gmail.com>
2014-05-20 23:47:22 +0530

    minor changes to header.php

--- public/header.php
+++ public/header.php
@@ -3,15 +3,15 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Video Volunteer</title>
+	<title>VV Workflow Tracker</title>
 </head>
 <link href="./main.css" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="buttons/css/buttons.css">
 <body>
 	<header>
 		<div class="nav">
-			<a href="index.php"><h1>VideoVolunteer</h1></a>
-			<a href="index.php"><h3>go home</h3></a>
+			<a href="index.php"><h1>VideoVolunteers</h1></a>
+			<a href="index.php"><h3>home</h3></a>
 
 		</div>
 	</header>
