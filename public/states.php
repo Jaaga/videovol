94f2720f916d9a12946593c38fada7c5a7ae36f9
StephanieBr <stephaniebrowne123@gmail.com>
2014-05-20 14:36:20 +0530

    changes to .php files

--- public/states.php
+++ public/states.php
@@ -9,18 +9,21 @@ mysql_select_db('videovol');
 	<title>states</title>
 </head>
 <body>
+<form method="POST" action="statesview.php">
 
 <?php
 $query = mysql_query("SELECT * FROM storytrack"); // Run the query
 
-echo '<label>View By:</label><select name="state"><option value="">Select State</option>'; // Opens the drop down box
+echo '<label>View By:</label><select name="state"><option value="">Select State</option>';
+ // Opens the drop down box
 
 // Loops through the query results, outputing the options one by one
 while ($row = mysql_fetch_array($query)) {
    echo '<option value="'.$row['state'].'">'.$row['state'].'</option>';
 }
-
 echo '</select>';// Closes drop down box
 ?>
+<input type="submit">
+</form>
 </body>
 </html>
---
+++ public/statesview.php
@@ -0,0 +1,44 @@
+<?php
+include '../model/dbcon.php';
+$db=dbopen();
+include ('header.php');
+//$data = "select * from storytrack where datereceived = '1986-12-23'"
+$state = $_POST['state'];
+echo "<table border='1'>
+<tr>
+<th>ccname</th>
+<th>state</th>
+<th>receiveddate</th>
+<th>issuetopic</th>
+<th>seq</th>
+<th>broll</th>
+<th>fint</th>
+<th>vo</th>
+<th>ptc</th>
+<th>cta</th>
+<th>vd</th>
+<th>translation</th>
+<th>statusedit</th>
+</tr>";
+$statusedit= "select * from storytrack where state = '$state'";
+$result = $db->query($statusedit);
+while($row = mysqli_fetch_array($result)) {
+  echo "<tr>";
+  echo "<td>" . $row['ccname'] . "</td>" ; 
+  echo "<td>" . $row['state'] . "</td>";
+  echo "<td>" . $row['receiveddate'] . "</td>";
+  echo "<td>" . $row['issuetopic'] . "</td>"; 
+  echo "<td>" . $row['seq'] . "</td>"; 
+  echo "<td>" . $row['broll'] . "</td>";
+  echo "<td>" . $row['fint'] . "</td>"; 
+  echo "<td>" . $row['vo'] . "</td>";
+  echo "<td>" . $row['ptc'] . "</td>"; 
+  echo "<td>" . $row['cta'] . "</td>";
+  echo "<td>" . $row['vd'] . "</td>";
+  echo "<td>" . $row['translation'] . "</td>";     
+  echo "</tr>";
+}
+
+
+echo "</table>";
+?>
