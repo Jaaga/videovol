e1c1d1d7f907df8695dbefe017250b037834bd48
Ramesh7128 <ramesh7128@gmail.com>
2014-06-10 17:06:39 +0530

    ff

--- model/StoryDB.php
+++ model/StoryDB.php
@@ -1,18 +1,21 @@
 <h1>hello</h1>
 
-<?php
-include '../model/dbcon.php';
+<?php include '../model/dbcon.php';
 
 function getAllBasicData() {
 	$db=dbopen();
 	$sql = "select ccname, state, receiveddate, issuetopic, storydescription, fid from storytrack";
 	$result = mysqli_query($db, $sql);	
 
-	$rows = array();
+	//$rows = array();
 	while($row = mysqli_fetch_array($result)) {
+    	echo $row;
     	$rows[] = $row;
+
 	}
 	return $rows;
 }
-
+var_dump($rows);
+$res = getAllBasicData();
+echo $res['ccname'];
 ?>
