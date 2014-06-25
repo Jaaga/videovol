ad14e150a3c599c400d5650d5fa6193ac1e24b13
Freeman Murray <freeman@jaaga.in>
2014-06-25 15:43:23 +0530

    fixed default data in storyeditor

--- controller/StoryController.php
+++ controller/StoryController.php
@@ -1,6 +1,8 @@
 <?php
 include_once('../model/StoryDB.php');
 
+echo $_GET['dateReceived'];
+
 addStory($_GET['ccname'], $_GET['state'], $_GET['dateReceived'], $_GET['issue'], 
 		 $_GET['story'], $_GET['unique_no'], $_GET['storydate'], $_GET['cc_pair'],
 		 $_GET['program'], $_GET['mentor'], $_GET['iu_topic'], $_GET['video_treatment'],
