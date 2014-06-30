1643e1e30a2d50cfef0f6b47600ece84f75e9380
Freeman Murray <freeman@jaaga.in>
2014-06-30 14:53:52 +0530

    taking search and tabs out of index.php - its in header.php now

--- public/index.php
+++ public/index.php
@@ -1,10 +1,11 @@
 <?php
-include_once("header.php");
 include_once("../model/StoryDB.php");
-
 if($_GET['action']) {
     include_once("../controller/StoryController.php");
 }
+
+include_once("header.php");
+
 ?>
 
 <!--
@@ -16,49 +17,6 @@ if($_GET['action']) {
 <a href="primeuser.php" class="button button-rounded button-flat-royal">Prime User</a>
 </div>
 -->
-
-<div><!--container for advanced search-->
-<table>
-    <form method="GET" action="index.php">
-    
- 	<tr><td valign="top"><label>Search : </label><select name="ccname"><option value="1">CCname</option>
-<?php	foreach(getCCNames() as $ccname) 
-            echo '<option value="'.$ccname.'">'.$ccname.'</option>';
-?>
-    </select></td>
-    
-<td valign="top"><select name="state"><option value="1">State</option>
-<?php   foreach(getStates() as $state) 
-        	echo '<option value="'.$state.'">'.$state.'</option>';
-?>
-	</select></td>
-        
-<td valign="top"><select name="issue"><option value="1">Issue</option>'; 
-<?php 	foreach(getIssues() as $issue) 
-        	echo '<option value="'.$issue.'">'.$issue.'</option>';
-?>
-</select></td>
-
-         
-        <td valign="top">From:<input type="text" name="fromdate" id="dateSearchFrom"></td>
-        <td valign="top">To:<input type="text" name="todate" id="searchToDate"></td>
-        <td><input type="submit" name="search" value="Search" /></td>
-        <!--<td><input type="submit"></td>-->
-    
-    </form>
-</table>
-</div>
-
-
-<div class="stages">
-<br>
-<a href="storyeditor.php" class="button button-rounded button-flat-primary">Create New Story</a>
-<a href="junioreditorview.php" class="button button-rounded button-flat-action">Mentor View</a>
-<a href="senioreditview.php" class="button button-rounded button-flat-action">Senior Editor View</a>
-<!--<a href="pmview.php" class="button button-rounded button-flat-action">Project Manager View</a>-->
-<a href="impactview1.php" class="button button-rounded button-flat-action">Impact View</a>
-<small><a href="viewalldata.php">View All Data</a></small>
-</div>
 
 <table class="table table-hover"><thead><tr><th></th><th>ID</th><th>CCName</th><th>State</th><th>Issue Topic</th><th>Received Date</th><th>Story Description</th><th>Stage</th><th>View Impact</th><tr>
 </thead>
