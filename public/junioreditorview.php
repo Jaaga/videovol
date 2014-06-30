a112f61bdc4d2f8e1b63b8556123548022385d7b
vaibhavmule <vaibhavmule135@gmail.com>
2014-06-30 16:06:22 +0530

    added date picker view pages

--- public/junioreditorview.php
+++ public/junioreditorview.php
@@ -48,3 +48,19 @@ foreach($junioreditorview as $row)
 echo "</table>";
 ?>
 
+<link rel="stylesheet" href="./jqueryui/css/ui/jquery-ui.css" />
+<script src="./jqueryui/js/jquery.js"></script>
+<script src="./jqueryui/js/jquery-ui.min.js"></script>
+<script>
+ $(function(){
+        $("#dateSearchFrom").datepicker({ dateFormat: 'yy-mm-dd' });
+        $("#searchToDate").datepicker({ dateFormat: 'yy-mm-dd' }).bind("change",function(){
+            var minValue = $(this).val();
+            minValue = $.datepicker.parseDate("yy-mm-dd", minValue);
+            minValue.setDate(minValue.getDate()+1);
+            $("#searchToDate").datepicker( "option", "minDate", minValue );
+        })
+    });
+</script>
+</body>
+</html>
