bc49a27ef374daca1941486e1ebcfe44eb6c4715
Freeman Murray <freeman@jaaga.in>
2014-07-02 16:09:17 +0530

    html util

---
+++ public/util.php
@@ -0,0 +1,42 @@
+<?php 
+
+function goodNotGoodSelect($label, $column, $value) {
+	echo "<tr><td valign='top'><label for='" . $column . "'>" . 
+		$label . " : " . $value . "</label></td>" .
+		"<td><select name='" . $column . "'>";
+	if (isset($value) == FALSE) 
+		echo "<option value='0'>Select</option>";
+	echo "<option value = 'Good'";
+	if($value == "Good") 
+		echo " selected"; 
+	echo ">Good</option>";
+	echo "<option value = 'Not Good'";
+	if($value == "Not Good")  
+		echo "selected";
+	echo ">Not Good</option>";
+	echo "
+		<br><label id='seqHint' style='color: red; font-style: italic;'></label>	
+		</td>
+		</tr>";
+}
+
+function yesNoSelect($label, $column, $value) {
+	echo "<tr><td valign='top'><label for='" . $column . "'>" . 
+		$label . " : " . $value . "</label></td>" .
+		"<td><select name='" . $column . "'>";
+	if (isset($value) == FALSE) 
+		echo "<option value='0'>Select</option>";
+	echo "<option value = 'Yes'";
+	if($value == "Yes") 
+		echo " selected"; 
+	echo ">Yes</option>";
+	echo "<option value = 'No'";
+	if($value == "No")  
+		echo "selected";
+	echo ">No</option>";
+	echo "
+		<br><label id='seqHint' style='color: red; font-style: italic;'></label>	
+		</td>
+		</tr>";
+}
+?>
