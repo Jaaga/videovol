21d107052571f0cc12b7064bc6de326deb4fac1c
Freeman Murray <freeman@jaaga.in>
2014-07-02 16:04:24 +0530

    impacteditor.php

--- public/index.php
+++ public/index.php
@@ -75,7 +75,7 @@ echo "<tr>
         <td>" .$row['storydescription'] . "</td>" .
         "<td><a href=index.php?stage=" . urlencode($row['stage']) . ">" .
         $row['stage'] . "</a></td>".
-        "<td><a href=preproduction/impact_team.php?id=" . urlencode($row['uniquenumber']) .">" . 
+        "<td><a href=impactEditor.php?id=" . urlencode($row['uniquenumber']) .">" . 
         "Impact" . "</a></td></tr>";
         }
 
