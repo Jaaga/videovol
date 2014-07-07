c5384322077986afbb592a467f6d2d719cdd5e43
setusaurabh <say2setu@gmail.com>
2014-07-07 15:29:27 +0530

    Changes in editor & editorcontroller

--- controller/editorcontroller.php
+++ controller/editorcontroller.php
@@ -1,17 +1,12 @@
 <?php 
-include '../model/PostDB.php';
-$un=$_POST['un'];
-if($_POST['uniquenumber'] && $_POST['editreceived'] && $_POST['editor'] && $_POST['editcomplete'] && $_POST['subtitlestatus'] && $_POST['videosent'] && $_POST['editnotes']!='0')
-{
-	$result = editorUpdate($_POST['seq'],$_POST['broll'],$_POST['fint'],$_POST['vo'],$_POST['ptc'],$_POST['cta'],$_POST['vd'],$_POST['translation'],$fid);
-	if(!$result)
-	{
-		echo "data not inserted";
-	}
-	else
-		header('location:../public/junioreditorview.php');
-}
-else
-echo "please select all the options one or more options not selected";
-	header("Refresh: 3; url=../public/junioreditor.php?id=".$fid);
+
+echo "<h1>in EC</h1>";
+
+include_once '../model/PostDB.php';
+if($_GET['action'] == 'update') {
+	editorUpdate($_GET['un'], $_GET['editreceived'], $_GET['editor'], $_GET['editcomplete'], 
+					   $_GET['subtitlestatus'],$_GET['videosent'],$_GET['editnotes']);
+//else
+//echo "please select all the options one or more options not selected";
+//	header("Refresh: 3; url=../public/junioreditor.php?id=".$fid);
 ?>
--- public/editor.php
+++ public/editor.php
@@ -1,7 +1,12 @@
 
 <?php
-include("header.php");
+include_once("header.php");
 $un=$_GET['un'];
+echo "edit UN: " . $un;
+
+include_once("../model/PostDB.php");
+
+$data = getEditorDataByUniqueNumber($un);
 ?>
 
 <!doctype html>
@@ -12,26 +17,26 @@ $un=$_GET['un'];
 </head>
 <body>
 	
-<form name="editor-form" method="post" >
-		<input name="fid" type="hidden" value="<?php echo $un; ?>" >
+<form name="editor-form" method="get" action="editview.php" onsubmit="javaScript:return validate_dropdown1();">
+		<input name="un" type="hidden" value=<?php echo $un; ?> > 
+		<input name="action" type="hidden" value="update" > 
 		
 	<h2>Editor's Dashboard</h2>
 
-<table>
+<table >
 	<tr><td valign="top">  
 		<label for="editreceived">Received for Editing</label>
 	</td>
 	<td valign="top">		
-		<input type="text" name="editreceived" id="datepick1"/>
+		<input type="text" name="editreceived" id="datepick1" value="<?php echo $data['editreceived']; ?>">
 	</td>
 	</tr>
 	
-	<tr>
-	<td valign="top"> 
-				<label for="editor">Editor :</label>
-			</td>
-			<td> 
-				<input type="text" name="editor">
+	<tr><td valign="top"> 
+		<label for="editor"> Editor :</label>
+	</td>
+	<td> 
+		<input type="text" name="editor" value="<?php echo $data['editor']; ?>">
 	</td>
 	</tr>
 	
@@ -39,7 +44,7 @@ $un=$_GET['un'];
 		<label for="editcomplete">Editing Completed</label>
 	</td>
 	<td valign="top">	
-		<input type="text" name="editcomplete" id="datepick2"/>
+		<input type="text" name="editcomplete" id="datepick2" value="<?php echo $data['editcomplete']; ?>">
 	</td>
 	</tr>
 
@@ -49,6 +54,7 @@ $un=$_GET['un'];
  	</td>
  	<td valign="top">
  		<select name="subtitlestatus">
+ 		<?php $value = $data['subtitlestatus'];?>
 			<option value ="0">Select</option>
 			<option value = "Done">Done</option>
 			<option value = "Not Done">Not Done</option>
@@ -60,7 +66,7 @@ $un=$_GET['un'];
 		<label for="videosent">Complete Video Sent</label>
 	</td>
 	<td valign="top">	
-		<input type="text" name="videosent" id="datepick3"/>
+		<input type="text" name="videosent" id="datepick3" value="<?php echo $data['videosent']; ?>">
 	</td>
 	</tr>
 
@@ -69,7 +75,7 @@ $un=$_GET['un'];
  		 <label for="editnotes">Notes: </label>
  	</td>
  	<td valign="top">
-  		<textarea  name="editnotes" maxlength="1000" cols="25" rows="6"></textarea>
+  		<textarea  name="editnotes" maxlength="1000" cols="25" rows="6" value="<?php echo $data['editnotes']; ?>"></textarea>
  	</td>
 	</tr>
  	
