58d6bc0341634bf58c2ba38f39e861ea0f62b261
Freeman Murray <freeman@jaaga.in>
2014-07-08 15:40:23 +0530

    simplify buttons

--- controller/JuniorEditorController.php
+++ controller/JuniorEditorController.php
@@ -1,13 +1,14 @@
 <?php 
 
 include_once '../model/PostDB.php';
-if($_GET['action'] == 'update') {
+if(isset($_GET['action'])) {
 	juniorEditorAddOrUpdate($_GET['un'], $_GET['footagereceived'], $_GET['wherereceived'], $_GET['reviewedby'], 
 					   $_GET['seq'],$_GET['broll'],$_GET['fint'],$_GET['vo'],$_GET['ptc'],
 					   $_GET['cta'],$_GET['vd'],
 		               $_GET['translation'],$_GET['paymentapproved'], $_GET['approvedon']);
+} else {
+
 }
-//else
 //echo "please select all the options one or more options not selected";
 //	header("Refresh: 3; url=../public/junioreditor.php?id=".$fid);
 ?>
